5c8db5845e96f0c31594e40dc0b150c470453111
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2021-03-14 12:58:00 -0400

    added backend and api JSON data in Stocks

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import axios from "axios";
+
+
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/Stocks.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="#stocks">
+    <h1>{{ stock }}</h1>
+    <p>this is test data</p>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      stock: "",
+      apiStock: "",
+    };
+  },
+  created: function() {
+    this.showStocks();
+    // this.apiStocks();
+  },
+  methods: {
+    showStocks: function() {
+      axios.get("/api/stocks/1").then(response => {
+        this.stock = response.data;
+        console.log("Realtime stock", this.stock);
+      });
+    },
+    apiStocks: function() {
+      axios
+        // .get("https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=")
+        .then(response => {
+          this.stock = response.data;
+          console.log("Realtime stock", this.apiStocks);
+        });
+    },
+  },
+};
+</script>
