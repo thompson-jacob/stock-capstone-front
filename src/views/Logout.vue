6bf6fb2c085247693e94bffba2744f018996bafb
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2024-01-17 11:34:25 -0700

    added logs to logout for debug

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -7,9 +7,11 @@ import axios from "axios";
 
 export default {
   created: function() {
+    console.log("Logging Out..");
     delete axios.defaults.headers.common["Authorization"];
     localStorage.removeItem("jwt");
     this.$router.push("/");
+    console.log("Logged out and redirected.");
   },
 };
 </script>
